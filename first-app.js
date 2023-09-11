22f4b2ce8f8c54c6e281ce835aac0eecd8c8cd0c
ibrahim-hisam <ibrahimhussam98@gmail.com>
2023-09-12 00:19:29 +0530

    Arrow function and Student Object

--- first-app.js
+++ first-app.js
@@ -1,1 +1,14 @@
-console.log("Hello World")
+//Arrow function returning the product of two variables
+const prodouct = (a, b) => a * b;
+console.log(prodouct(2,4));
+
+// Student Object
+const student = {
+    name: 'Ibrahim',
+    age: 23,
+    greet(){
+    console.log('Hi, My name is ' + this.name + ", my age is " + this.age);
+    }
+};  
+
+student.greet();
