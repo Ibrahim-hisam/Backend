2a90026db482d317030f11221a088b0f72257ca5
ibrahim-hisam <ibrahimhussam98@gmail.com>
2023-09-13 20:45:18 +0530

    add-product

--- file.js
+++ file.js
@@ -1,18 +1,25 @@
 const http = require('http');
 
 const express = require('express');
+const body = require('body-parser');
+
 const app = express();
 
-app.use((req, res,next) => {
+app.use(body.urlencoded({extended: false}));
 
-    console.log('In the middleware!');
-    next();
+
+app.use('/add-product',(req, res,next) => {
+    res.send('<form action="/product" method="POST"><input type="text" name="title" > <input type="text" name="size"><button type="submit">Add Product</button></form>')
 });
 
-app.use((req, res,next) => {
+app.post('/product',(req,res,next) => {
+    console.log(req.body);
+    res.redirect('/');
+});
 
-    console.log('In another  middleware!');
+
+app.use('/',(req, res,next) => {
     res.send('<h1>Hello from Express!</h1>')
 });
 
-app.listen(4000);
+app.listen(3000);
