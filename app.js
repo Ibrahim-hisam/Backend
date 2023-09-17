0bf94dcb73c632c3c58e169da075b490f6b9a26b
ibrahim-hisam <ibrahimhussam98@gmail.com>
2023-09-17 06:06:32 +0530

    Sequelize

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const errorController = require('./controllers/error');
-const db = require('./util/database');
+const sequelize = require('./util/database');
 
 const app = express();
 
@@ -15,7 +15,6 @@ const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
 
-
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -24,4 +23,13 @@ app.use(shopRoutes);
 
 app.use(errorController.get404);
 
-app.listen(8000);
+sequelize
+.sync()
+.then(result => {
+    app.listen(8000);
+})
+.catch(err =>{
+    console.log(err);
+});
+
+
